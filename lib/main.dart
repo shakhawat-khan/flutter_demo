d90231eadc4f72a5564c2102348ed2939bc2bb8d
shakhawat-khan <satanfromhelll666@gmail.com>
2020-12-22 16:20:08 +0600

    hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,36 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text('hello'),
         centerTitle: true,
+        backgroundColor: Colors.red[600],
       ),
       body:Center(
-        child: Text('hello bros'),
+        child: Text('hello bros 2',
+          style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600]
+          ),),
       ),
-    ),
 
-  ));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+
+  }
 }
+
