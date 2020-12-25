b35787cf6e8f6d677b2afaeb6f182ed8c353be82
shakhawat-khan <satanfromhelll666@gmail.com>
2020-12-26 02:20:10 +0600

    ID project

--- lib/main.dart
+++ lib/main.dart
@@ -2,49 +2,102 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),
+    home: NinjaCard(),
   ));
 }
 
-class Home extends StatelessWidget {
+class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text('hello'),
-
+        title: Text('Ninja ID Card'),
         centerTitle: true,
-        backgroundColor: Colors.red[600],
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
       ),
-      body: Row(
-        children: <Widget>[
-           Expanded(
-             flex: 2,
-             child: Container(
-               padding: EdgeInsets.all(10.0),
-               color: Colors.cyan,
-               child: Text('1'),
-             ),
-           ),
-          Expanded(
-            flex: 3,
-            child: Container(
-              padding: EdgeInsets.all(10.0),
-              color: Colors.pinkAccent,
-              child: Text('1'),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children:<Widget> [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/tommy.jpg'),
+                radius: 40.0,
+              ),
             ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(10.0),
-              color: Colors.lightGreen,
-              child: Text('1'),
+            Divider(
+              height: 90.0,
+              color: Colors.grey[800],
             ),
-          )
-        ],
-      )
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 05.0),
+            Text(
+              'Shakhawat',
+              style: TextStyle(
+                color: Colors.amberAccent,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height: 50.0),
 
+            Text(
+              'CURRENT NINJA LEVEL',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0),
+
+            Text(
+              '100',
+              style: TextStyle(
+                  color: Colors.amberAccent,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+
+            Row(
+
+              children: <Widget>[
+                SizedBox(height: 150.0),
+                Icon(
+
+                  Icons.email,
+                  color: Colors.grey,
+
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'shakhawatkhan123@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+
+                  ),
+                ),
+
+
+              ],
+            )
+
+          ],
+        ),
+      ),
     );
 
   }
